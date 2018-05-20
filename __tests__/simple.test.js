7cd45b64b9ac8b1e745a8cc25d8e74f7d0382649
Jon Scheiding <jonscheiding@gmail.com>
2018-05-19 21:55:34 -0400

    POC getting messages from the websocket

--- __tests__/simple.test.js
+++ __tests__/simple.test.js
@@ -1,11 +1,14 @@
 import Nightmare from 'nightmare';
 import dotenv from 'dotenv';
+import WebSocket from 'ws';
+
+/* eslint-disable no-console */
 
 dotenv.config();
-jest.setTimeout(100000);
+jest.setTimeout(30000);
 
 test(`Aristotle's law of identity`, async () => {
-  const browser = new Nightmare({show: true, typeInterval: 1})
+  const browser = new Nightmare({typeInterval: 1})
     .goto('https://graph.api.smartthings.com/ide/logs')
     .type('#username', process.env.SMARTTHINGS_USERNAME)
     .click('#next-step-btn')
@@ -15,8 +18,24 @@ test(`Aristotle's law of identity`, async () => {
     .wait('#ide-console');
 
   const cookies = await browser.cookies.get();
+  /* eslint-disable-next-line no-undef */
   const st = await browser.evaluate(() => ST);
   await browser.end();
 
-  console.log({cookies, st});
+  const cookieHeader = cookies.map((c) => c.name + '=' + c.value).join(';');
+
+  const socket = new WebSocket(
+    st.globals.websocket + 'client/' + st.globals.client,
+    {
+      headers: {
+        'Cookie': cookieHeader,
+      },
+    }
+  );
+
+  socket.on('message', console.log);
+
+  await new Promise((resolve) => setTimeout(resolve, 20000));
+
+  socket.close();
 });
