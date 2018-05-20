4870dc1cd82b8ed6bf5293a5b22cd659bfd215c0
Jon Scheiding <jonscheiding@gmail.com>
2018-05-19 21:28:52 -0400

    POC getting info we need from ST login

--- __tests__/simple.test.js
+++ __tests__/simple.test.js
@@ -1,3 +1,22 @@
-test(`Aristotle's law of identity`, () => {
-  expect(1).toBe(1);
+import Nightmare from 'nightmare';
+import dotenv from 'dotenv';
+
+dotenv.config();
+jest.setTimeout(100000);
+
+test(`Aristotle's law of identity`, async () => {
+  const browser = new Nightmare({show: true, typeInterval: 1})
+    .goto('https://graph.api.smartthings.com/ide/logs')
+    .type('#username', process.env.SMARTTHINGS_USERNAME)
+    .click('#next-step-btn')
+    .wait('#password')
+    .type('#password', process.env.SMARTTHINGS_PASSWORD)
+    .click('#login-user-btn')
+    .wait('#ide-console');
+
+  const cookies = await browser.cookies.get();
+  const st = await browser.evaluate(() => ST);
+  await browser.end();
+
+  console.log({cookies, st});
 });
