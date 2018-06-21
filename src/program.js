4c561b310934b35ee00f1b713bfef68a2c5d0008
Jon Scheiding <jonscheiding@gmail.com>
2018-06-21 13:00:03 -0400

    Added beginnings of a CLI

---
+++ src/program.js
@@ -0,0 +1,31 @@
+import dotenv from 'dotenv';
+import yargs from 'yargs';
+
+import { LoginService } from './login-service';
+
+dotenv.config();
+
+export default function run(args) {
+  const options = yargs
+    .env('SMARTTHINGS')
+    .option('username', {
+      alias: 'u',
+      describe: 'SmartThings username',
+    })
+    .option('password', {
+      alias: 'p',
+      describe: 'SmartThings password',
+    })
+    .option('show-browser', {
+      describe: 'Show the browser window when logging in',
+    })
+    .demandOption(['username', 'password'])
+    .boolean('show-browser')
+    .help()
+    .parse(args);
+
+  const loginService = new LoginService(options.showBrowser);
+  loginService.login(options.username, options.password)
+    .then(console.log)
+    .catch(console.log);
+}
