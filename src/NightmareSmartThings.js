52b5ecff7aad8e1ae488ef0aaf80603b6aed7474
Jon Scheiding <jonscheiding@gmail.com>
2018-06-22 14:32:21 -0400

    Use insert() instead of type() per recommendation in NightmareJS docs

--- src/NightmareSmartThings.js
+++ src/NightmareSmartThings.js
@@ -7,10 +7,10 @@ export default class NightmareSmartThings extends Nightmare {
     return this
       .goto(IDE_CONSOLE_URL)
       .wait('#username')
-      .type('#username', username)
+      .insert('#username', username)
       .click('#next-step-btn')
       .wait('#password')
-      .type('#password', password)
+      .insert('#password', password)
       .click('#login-user-btn')
       .wait('#ide-console');
   }
