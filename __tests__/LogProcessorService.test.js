766c0892404b21e111429d11df85c64ed01ef443
Jon Scheiding <jonscheiding@gmail.com>
2018-06-22 11:37:36 -0400

    Added describe()'s to tests

--- __tests__/LogProcessorService.test.js
+++ __tests__/LogProcessorService.test.js
@@ -5,58 +5,60 @@ let emitter;
 let handler;
 let service;
 
-beforeEach(() => {
-  emitter = new EventEmitter();
-  service = new LogProcessorService(emitter);
-  handler = jest.fn(console.log);
+describe('LoginProcessorService', () => {
+  beforeEach(() => {
+    emitter = new EventEmitter();
+    service = new LogProcessorService(emitter);
+    handler = jest.fn(console.log);
 
-  service.on('logs', handler);
-});
-
-test('constructor creates an instance', () => {
-  expect(service).toBeInstanceOf(LogProcessorService);
-});
-
-test('raises non-JSON as msg property', () => {
-  emitter.emit('message', 'Hello');
-
-  expect(handler.mock.calls.length).toBe(1);
-  expect(handler.mock.calls[0][0]).toMatchObject({
-    msg: 'Hello',
+    service.on('logs', handler);
   });
-});
 
-test("raises JSON object as-is if it doesn't have a 'logs' property", () => {
-  const message = {
-    string: 'something',
-    boolean: true,
-    nested: {
-      string: 'something else',
-    },
-  };
-  emitter.emit('message', JSON.stringify(message));
-
-  expect(handler.mock.calls.length).toBe(1);
-  expect(handler.mock.calls[0][0]).toMatchObject(message);
-});
-
-test("flattens JSON object that has a 'logs' property", () => {
-  const message = {
-    string: 'something',
-    logs: [
-      { message: 'first message' },
-      { message: 'second message' },
-    ],
-  };
-  emitter.emit('message', JSON.stringify(message));
-
-  expect(handler.mock.calls.length).toBe(2);
-  expect(handler.mock.calls[0][0]).toMatchObject({
-    string: 'something',
-    message: 'first message',
+  test('constructor creates an instance', () => {
+    expect(service).toBeInstanceOf(LogProcessorService);
   });
-  expect(handler.mock.calls[1][0]).toMatchObject({
-    string: 'something',
-    message: 'second message',
+
+  test('raises non-JSON as msg property', () => {
+    emitter.emit('message', 'Hello');
+
+    expect(handler.mock.calls.length).toBe(1);
+    expect(handler.mock.calls[0][0]).toMatchObject({
+      msg: 'Hello',
+    });
+  });
+
+  test("raises JSON object as-is if it doesn't have a 'logs' property", () => {
+    const message = {
+      string: 'something',
+      boolean: true,
+      nested: {
+        string: 'something else',
+      },
+    };
+    emitter.emit('message', JSON.stringify(message));
+
+    expect(handler.mock.calls.length).toBe(1);
+    expect(handler.mock.calls[0][0]).toMatchObject(message);
+  });
+
+  test("flattens JSON object that has a 'logs' property", () => {
+    const message = {
+      string: 'something',
+      logs: [
+        { message: 'first message' },
+        { message: 'second message' },
+      ],
+    };
+    emitter.emit('message', JSON.stringify(message));
+
+    expect(handler.mock.calls.length).toBe(2);
+    expect(handler.mock.calls[0][0]).toMatchObject({
+      string: 'something',
+      message: 'first message',
+    });
+    expect(handler.mock.calls[1][0]).toMatchObject({
+      string: 'something',
+      message: 'second message',
+    });
   });
 });
--- __tests__/LoginService.test.js
+++ __tests__/LoginService.test.js
@@ -23,85 +23,87 @@ jest.mock('../src/NightmareSmartThings', () =>
     gotoIdeConsole: mockGotoIdeConsole,
   })));
 
-beforeEach(() => {
-  mockGotoIdeConsole = jest.fn(() => ({
-    end: mockEnd,
-    cookies: { get: mockCookiesGet },
-    evaluate: mockEvaluate,
-  }));
+describe('LoginService', () => {
+  beforeEach(() => {
+    mockGotoIdeConsole = jest.fn(() => ({
+      end: mockEnd,
+      cookies: { get: mockCookiesGet },
+      evaluate: mockEvaluate,
+    }));
 
-  mockEnd = () => Promise.resolve();
-  mockCookiesGet = () => Promise.resolve(correctCookies);
-  mockEvaluate = () => Promise.resolve(correctEvaluateResult);
-});
+    mockEnd = () => Promise.resolve();
+    mockCookiesGet = () => Promise.resolve(correctCookies);
+    mockEvaluate = () => Promise.resolve(correctEvaluateResult);
+  });
 
-test('constructor creates an instance', () => {
-  const service = new LoginService();
+  test('constructor creates an instance', () => {
+    const service = new LoginService();
 
-  expect(service).toBeInstanceOf(LoginService);
-});
+    expect(service).toBeInstanceOf(LoginService);
+  });
 
-test('login() throws if username or password are not provided', () => {
-  const service = new LoginService();
-  const loginResult = service.login();
+  test('login() throws if username or password are not provided', () => {
+    const service = new LoginService();
+    const loginResult = service.login();
 
-  return expect(loginResult).rejects
-    .toThrow('Username and password are required.');
-});
+    return expect(loginResult).rejects
+      .toThrow('Username and password are required.');
+  });
 
-test("login() throws if the page didn't return the expected cookies", () => {
-  mockCookiesGet = () => Promise.resolve([
-    { name: 'SOME_OTHER_COOKIE', value: 'SOME_OTHER_COOKIE cookie' },
-  ]);
+  test("login() throws if the page didn't return the expected cookies", () => {
+    mockCookiesGet = () => Promise.resolve([
+      { name: 'SOME_OTHER_COOKIE', value: 'SOME_OTHER_COOKIE cookie' },
+    ]);
 
-  const service = new LoginService();
-  const loginResult = service.login('username', 'password');
+    const service = new LoginService();
+    const loginResult = service.login('username', 'password');
 
-  return expect(loginResult).rejects
-    .toThrow(/Expected cookies to include JSESSIONID and _JTKN/);
-});
+    return expect(loginResult).rejects
+      .toThrow(/Expected cookies to include JSESSIONID and _JTKN/);
+  });
 
-test("login() throws if the page doesn't have the expected JS content", () => {
-  mockEvaluate = () => Promise.resolve({ globals: {} });
+  test("login() throws if the page doesn't have the expected JS content", () => {
+    mockEvaluate = () => Promise.resolve({ globals: {} });
 
-  const service = new LoginService();
-  const loginResult = service.login('username', 'password');
+    const service = new LoginService();
+    const loginResult = service.login('username', 'password');
 
-  return expect(loginResult).rejects
-    .toThrow(/Unexpected result evaluating the console page/);
-});
+    return expect(loginResult).rejects
+      .toThrow(/Unexpected result evaluating the console page/);
+  });
 
-test('login() returns the right cookies and URL if the login is successful', async () => {
-  const service = new LoginService();
-  const loginResult = await service.login('username', 'password');
+  test('login() returns the right cookies and URL if the login is successful', async () => {
+    const service = new LoginService();
+    const loginResult = await service.login('username', 'password');
 
-  expect(loginResult.headers.Cookie).toBe('JSESSIONID=JSESSIONID cookie;_JTKN=_JTKN cookie;SOME_OTHER_COOKIE=SOME_OTHER_COOKIE cookie');
-  expect(loginResult.url).toBe('wss://some-websocket-url/client/some_client_guid');
-});
+    expect(loginResult.headers.Cookie).toBe('JSESSIONID=JSESSIONID cookie;_JTKN=_JTKN cookie;SOME_OTHER_COOKIE=SOME_OTHER_COOKIE cookie');
+    expect(loginResult.url).toBe('wss://some-websocket-url/client/some_client_guid');
+  });
 
-test('login() passes the username and password to the browser call', async () => {
-  const service = new LoginService();
-  await service.login('username', 'password');
+  test('login() passes the username and password to the browser call', async () => {
+    const service = new LoginService();
+    await service.login('username', 'password');
 
-  expect(mockGotoIdeConsole.mock.calls[0]).toEqual(['username', 'password']);
-});
+    expect(mockGotoIdeConsole.mock.calls[0]).toEqual(['username', 'password']);
+  });
 
-test('login() calls end() on the request', async () => {
-  mockEnd = jest.fn();
+  test('login() calls end() on the request', async () => {
+    mockEnd = jest.fn();
 
-  const service = new LoginService();
-  await service.login('username', 'password');
+    const service = new LoginService();
+    await service.login('username', 'password');
 
-  expect(mockEnd.mock.calls.length).toBe(1);
-});
+    expect(mockEnd.mock.calls.length).toBe(1);
+  });
 
-test('login() calls end() on the request even when there was an error', async () => {
-  mockEnd = jest.fn();
-  mockEvaluate = () => Promise.reject(new Error());
+  test('login() calls end() on the request even when there was an error', async () => {
+    mockEnd = jest.fn();
+    mockEvaluate = () => Promise.reject(new Error());
 
-  const service = new LoginService();
-  const loginResult = service.login('username', 'password');
+    const service = new LoginService();
+    const loginResult = service.login('username', 'password');
 
-  await expect(loginResult).rejects.toBeDefined();
-  expect(mockEnd.mock.calls.length).toBe(1);
+    await expect(loginResult).rejects.toBeDefined();
+    expect(mockEnd.mock.calls.length).toBe(1);
+  });
 });
