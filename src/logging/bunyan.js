4d3a9d7d6a8e355179e6fb07ed9cc73285aec2cf
Jon Scheiding <jonscheiding@gmail.com>
2018-07-04 21:40:13 -0400

    #4 Enabled logging to Seq if configured

--- src/logging/bunyan.js
+++ src/logging/bunyan.js
@@ -1,5 +1,21 @@
 import bunyan from 'bunyan';
+import bunyanSeq from 'bunyan-seq';
 
-export default bunyan.createLogger({
+const bunyanConfig = {
   name: 'smartthings',
-});
+  streams: [
+    {
+      stream: process.stdout,
+      level: 'debug',
+    },
+  ],
+};
+
+if (process.env.SEQ_INGEST_URL) {
+  bunyanConfig.streams.push(bunyanSeq.createStream({
+    serverUrl: process.env.SEQ_INGEST_URL,
+    level: 'debug',
+  }));
+}
+
+export default bunyan.createLogger(bunyanConfig);
